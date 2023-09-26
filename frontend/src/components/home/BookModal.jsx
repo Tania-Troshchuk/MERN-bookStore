c5b0fe163227f9bdd84b648dda3a250dc6f8c0a4
Tania Troshchuk <troschuk95@gmail.com>
2023-09-26 13:39:31 +0300

    added prevent body scroll on open Modal box

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -1,8 +1,17 @@
 import { PiBookOpenTextLight } from 'react-icons/pi'
 import { BiUserCircle } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'
+import { useEffect } from 'react'
 
 export const BookModal = ({ book, onClose }) => {
+  document.body.classList.add('overflow-hidden')
+
+  useEffect(() => {
+    return () => {
+      document.body.classList.remove('overflow-hidden')
+    }
+  }, [])
+
   return (
     <div className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
       onClick={onClose}
